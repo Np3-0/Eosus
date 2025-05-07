21fabb7e4b7ba55899000b74a2167060f3c81435
Np3-0 <obnathan3@gmail.com>
2025-05-06 23:27:52 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Layout from "./components/Layout";
+import Hero from "./components/sections/Hero";
+
+export default function App() {
+  return (
+    <>
+      <Layout title="Eosus">
+        <Hero />
+      </Layout>
+    </>
+  )
+}
---
+++ src/components/shared/BtnLink.tsx
@@ -0,0 +1,13 @@
+interface BtnLinkProps {
+    href: string;
+    text: string;
+    className?: string;
+}
+
+export default function BtnLink({ href, text, className = "" }: BtnLinkProps) {
+    return(
+        <a href={href} className={`px-6 py-3 rounded-full outline-none relative overflow-hidden dark:bg-cordovan cursor-pointer transform transition duration-300 hover:scale-[1.02] ${className}`}>
+            <span className="relative z-10 text-white ">{text}</span>
+        </a>
+    );
+}
