1f066e219cfa60af89fa91b9efd8034c9d689f5f
Nate-OBrien <nathan.obrien@mcvts.org>
2025-09-26 12:59:56 -0400

    layout for ai page

--- src/components/pages/AI.tsx
+++ src/components/pages/AI.tsx
@@ -8,6 +8,8 @@ import { onAuthStateChanged } from "firebase/auth";
 import { auth, db } from "../../config/firebase";
 import { doc, getDoc } from "firebase/firestore";
 import checkUserStatus from "../../utils/checkUserStatus";
+import Paragraph from "../shared/Paragraph";
+import Button from "../shared/Button";
 
 export default function AI() {
     const navigate = useNavigate();
@@ -26,41 +28,63 @@ export default function AI() {
         location: null,
     });
 
-     useEffect(() => {
-            const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
-                setUser(firebaseUser);
-                if (firebaseUser) {
-                    checkUserStatus(navigate);
-                } else {
-                    navigate("/signup");
-                }
-            });
-            return () => unsubscribe();
-        }, [navigate]);
-    
-        useEffect(() => {
-            if (!user) return;
-            const fetchUserData = async () => {
-                const docRef = doc(db, "users", user.uid);
-                const docSnap = await getDoc(docRef);
-                if (docSnap.exists()) {
-                    setUserObj(
-                        docSnap.data() as {
-                            name: string;
-                            img: string;
-                            email: string;
-                            privacy: boolean;
-                            location: string | null;
-                        });
-                }
-            };
-            fetchUserData();
-        }, [user]);
-        
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+            setUser(firebaseUser);
+            if (firebaseUser) {
+                checkUserStatus(navigate);
+            } else {
+                navigate("/signup");
+            }
+        });
+        return () => unsubscribe();
+    }, [navigate]);
+
+    useEffect(() => {
+        if (!user) return;
+        const fetchUserData = async () => {
+            const docRef = doc(db, "users", user.uid);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                setUserObj(
+                    docSnap.data() as {
+                        name: string;
+                        img: string;
+                        email: string;
+                        privacy: boolean;
+                        location: string | null;
+                    });
+            }
+        };
+        fetchUserData();
+    }, [user]);
+
     return (
         <Layout navType={1} img={userObj.img} email={userObj.email} name={userObj.name}>
             <Container className="min-h-screen flex flex-col mt-48 items-center">
-                <Title>AI PAGE</Title>
+                <Title>Meet Pranny</Title>
+                <img src="/ai_logo.svg" alt="Pranny" className="w-48 h-48 mb-4" />
+                <Paragraph className="font-semibold">An AI assistant programmed to help you stay alert with natural disasters.</Paragraph>
+                <div className="mt-10 w-3/4 flex">
+                    <div className="flex sm:flex-row flex-col gap-5 w-full">
+                        <form 
+                            onSubmit={(e) => alert(e)} 
+                            className="py-1 pl-6 w-full pr-1 flex gap-3 items-center text-heading-3
+                                        shadow-lg shadow-box-shadow border border-box-border
+                                        bg-box-bg rounded-full ease-linear focus-within:bg-body
+                                        focus-within:border-primary"
+                        >
+                            <input
+                                type="text"
+                                placeholder="What should I do during a flood?"
+                                required
+                                className="w-full py-3 outline-none bg-transparent" />
+                            <Button type="submit" className="min-w-max text-white transform transition duration-300 hover:scale-[1.02]">
+                                <span className="relative z-[5]">Ask Pranny</span>
+                            </Button>
+                        </form>
+                    </div>
+                </div>
             </Container>
         </Layout>
     );
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,6 +4,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAI, getGenerativeModel, GoogleAIBackend } from "firebase/ai";
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -26,3 +28,5 @@ export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const ai = getAI(app, { backend: new GoogleAIBackend() });
+export const model = getGenerativeModel(ai, { model: "gemini-2.5-flash" });
